87d28a5ba501f70ca3d18e0bbce9fd090c6e5647
Jack Atfield <109289488+J-Atfield@users.noreply.github.com>
2024-08-19 10:54:57 +0100

    made basic underlying asset class, simplified option constructor

--- option.py
+++ option.py
@@ -1,21 +1,16 @@
 import tree
 import numpy as np
-from collections import defaultdict
+from underlying import Underlying
 
 class Option():
-    def __init__(self, S0, K, r, T, vol, n, option_type='call'):
-        self.S0 = S0
+    def __init__(self, S0, K, T, vol, r, n, option_type='call',):
+        self.underlying = Underlying(S0, T, n, vol, r) # has S0, 
+        self.type = option_type
         self.K = K
         self.T = T
-        self.dt = T / n
         self.r = r
-        self.a = np.round(np.exp(r * self.dt), 4)
-        self.u = np.round(np.exp(vol * np.sqrt(self.dt)), 4)
-        self.d = np.round(np.exp(-vol * np.sqrt(self.dt)), 4)
-        self.p = (self.a - self.d) / (self.u - self.d)
-        self.type = option_type
-        self.tree = tree.Tree(S0, n, self.u, self.d, self.a)
-        self.option_price = self.calculate_option_price()
+        self.n = n
+        self.price = self.calculate_option_price()
 
     def payoff(self, S):
         if self.type == 'call':
@@ -24,7 +19,8 @@ class Option():
             return max(self.K - S, 0)
     
     def calculate_option_price(self):
-        levels = self.tree.get_tree_levels(self.tree.root)
+        p = self.underlying.p
+        levels = self.underlying.tree.get_tree_levels(self.underlying.tree.root)
         max_depth = max(levels.keys())
 
         option_values = {max_depth: [self.payoff(S) for S in levels[max_depth]]}
@@ -32,6 +28,6 @@ class Option():
         for depth in range(max_depth - 1, -1, -1):
             option_values[depth] = []
             for i in range(len(levels[depth])):
-                option_values[depth].append((self.p * option_values[depth + 1][i+1] + (1 - self.p) * option_values[depth + 1][i]))
+                option_values[depth].append((p * option_values[depth + 1][i+1] + (1 - p) * option_values[depth + 1][i]))
 
         return option_values[0][0] * np.exp(-self.r * self.T)
