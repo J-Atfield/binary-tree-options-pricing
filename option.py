838b35817db7626a411730789b611882ad001e25
Jack Atfield <109289488+J-Atfield@users.noreply.github.com>
2024-08-19 23:57:21 +0100

    updated option to have (option) pricing tree instead of only price, added function to compute delta of option at each step and build delta tree

--- option.py
+++ option.py
@@ -1,5 +1,7 @@
 import numpy as np
+from collections import defaultdict
 from underlying import Underlying
+from tree import Tree
 
 class Option():
     def __init__(self, S0, K, T, vol, r, n, option_type='call',):
@@ -9,7 +11,8 @@ class Option():
         self.T = T
         self.r = r
         self.n = n
-        self.price = self.calculate_option_price()
+        self.price_tree = Tree(type='option', levels=self.calculate_option_prices())
+        self.delta_tree = Tree(type='delta', levels=self.calculate_option_deltas())
 
     def payoff(self, S):
         if self.type == 'call':
@@ -17,7 +20,7 @@ class Option():
         elif self.type == 'put':
             return max(self.K - S, 0)
     
-    def calculate_option_price(self):
+    def calculate_option_prices(self):
         p = self.underlying.p
         levels = self.underlying.tree.get_tree_levels(self.underlying.tree.root)
         max_depth = max(levels.keys())
@@ -29,4 +32,20 @@ class Option():
             for i in range(len(levels[depth])):
                 option_values[depth].append((p * option_values[depth + 1][i+1] + (1 - p) * option_values[depth + 1][i]))
 
-        return option_values[0][0] * np.exp(-self.r * self.T)
+        option_values[0][0] = option_values[0][0] * np.exp(-self.r * self.T)
+
+        return option_values #[0][0] * np.exp(-self.r * self.T)
+    
+    def calculate_option_deltas(self):
+        option_levels = self.price_tree.get_tree_levels(self.price_tree.root)
+        price_levels = self.underlying.tree.get_tree_levels(self.underlying.tree.root)
+        max_depth = max(option_levels.keys())
+
+        option_deltas = defaultdict(list)
+        
+        for depth in range(max_depth, -1, -1):
+            for i in range(len(option_levels[depth]) - 1):
+                option_deltas[depth-1].append((option_levels[depth][i+1] - option_levels[depth][i]) / (price_levels[depth][i+1] - price_levels[depth][i]))
+
+        return option_deltas
+        
