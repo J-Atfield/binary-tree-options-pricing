84f4fdb8dd1d48b9678cbaab3a5613c29a455ea8
Jack Atfield <109289488+J-Atfield@users.noreply.github.com>
2024-08-20 17:51:52 +0100

    moved tree plotting from notebook to option class

--- option.py
+++ option.py
@@ -1,5 +1,7 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from collections import defaultdict
+from scipy.sparse import lil_matrix
 from underlying import Underlying
 
 class Option():
@@ -50,5 +52,38 @@ class Option():
                 option_deltas[depth-1].append((option_levels[depth][i+1] - option_levels[depth][i]) / (price_levels[depth][i+1] - price_levels[depth][i]))
 
         return option_deltas
+    
+    def plot_tree(self, type):
+        nb_steps = self.n
 
+        levels = None
+        if type == 'option':
+            levels = self.price_tree
+        elif type == 'delta':
+            levels = self.delta_tree
+            nb_steps -= 1
+        elif type == 'underlying':
+            levels = self.underlying_tree
+        
+        mat = lil_matrix((nb_steps+1, nb_steps*2+1), dtype = np.float32)
+        
+        for i in range(nb_steps+1):
+            value = levels[i]
+            rows = np.zeros(i+1, dtype=np.int16) + i
+            cols = np.arange(i+1)*2+nb_steps-i
+            mat[rows, cols] = value
+        
+        fig = plt.figure(figsize=[5, 5])
+        
+        plt.axis('off')
+        plt.title(f'{type} Tree')
+        
+        for i in range(nb_steps):
+            x = (np.arange(3 + i*2) % 2 == 0) + i
+            y = np.arange(0, 2*(i+1)+1) - 1 + (nb_steps-i)
+            v = mat[x, y].toarray()[0]
+            plt.plot(x, v, 'bo-')
+        
+            for xi, vi in zip(x, v):
+                plt.text(xi, vi, f'{vi:.2f}', fontsize=8, ha='center', va='bottom')
         
